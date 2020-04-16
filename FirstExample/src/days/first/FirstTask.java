1b9dddf9c65c3efcd424a3282ad3ad088ec733e9
Mantasma <mant4s.mazeika@gmail.com>
2020-04-16 19:13:48 +0300

    pirmas commitas

---
+++ FirstExample/src/days/first/FirstTask.java
@@ -0,0 +1,39 @@
+package days.first;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        System.out.println("Iveskite zodi");
+        Scanner sc = new Scanner(System.in);
+        String word;
+        do {
+            System.out.println("Iveskite zodi");
+            word = sc.nextLine();
+
+            checkOrWordIsEvent(word);
+            System.out.println("A raidziu zodyje yra:" + countLetters(word, 'a'));
+
+        } while (!"pabaiga".equalsIgnoreCase(word));
+    }
+
+    private static void checkOrWordIsEvent(String word) {
+        if (word.length() % 2 == 0) {
+            System.out.println(String.format("Ivestas zodis %s yra lyginis", word));
+
+        } else {System.out.println(String.format("Ivestas zodis %s yra nelyginis", word));
+        }
+    }
+    private static int countLetters(String word, char letter){
+        int count = 0;
+        for(int i=0; i< word.length(); i++){
+            if(letter == word.charAt(i)){
+                count++;
+
+            }
+        }
+        return count;
+    }
+}
+
---
+++ FirstExample/src/days/first/SecondTask.java
@@ -0,0 +1,27 @@
+package days.first;
+
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        System.out.println("Iveskite norima polidroma");
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine().replaceAll(" ", "");
+        if(isPalindrome(word)){
+            System.out.println("Palidrome");
+        } else {
+            System.out.println("word is not a palidrome");
+        }
+
+    }
+
+    private static boolean isPalindrome(String word){
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) != word.charAt(word.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
