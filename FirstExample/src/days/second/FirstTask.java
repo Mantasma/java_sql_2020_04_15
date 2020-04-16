d91697e035078cfaa3f43851797c063a92870057
Mantasma <mant4s.mazeika@gmail.com>
2020-04-16 21:54:53 +0300

    add catch

--- FirstExample/src/days/second/FirstTask.java
+++ FirstExample/src/days/second/FirstTask.java
@@ -1,66 +1,88 @@
 package days.second;
 
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FirstTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         FirstTask firstTask = new FirstTask();
-        System.out.println("1. Staciojo trikampo plotas\n2. Staciakampio plotas\n3. Kvadrato plotas\n4. Apskritimo plotas\n5. Baigti");
+        int selection = 0;
+        while (selection != 5) {
+            System.out.println("1. Staciojo trikampo plotas\n2. Staciakampio plotas\n3. Kvadrato plotas\n4. Apskritimo plotas\n5. Baigti");
 
-        int selection = scanner.nextInt();
+            selection = (int) firstTask.getCorrectNumber(scanner);
 
-        switch(selection){
-            case 1:
-                firstTask.countTriangle(scanner);
-                break;
-            case 2:
-                firstTask.countRectangle(scanner);
-                break;
-            case 3:
-                firstTask.countSquare(scanner);
-                break;
-            case 4:
-                firstTask.countCircle(scanner);
-                break;
-            case 5:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("tokio veikmso nera");
+            switch (selection) {
+                case 1:
+                    firstTask.countTriangle(scanner);
+                    break;
+                case 2:
+                    firstTask.countRectangle(scanner);
+                    break;
+                case 3:
+                    firstTask.countSquare(scanner);
+                    break;
+                case 4:
+                    firstTask.countCircle(scanner);
+                    break;
+                case 5:
+                    System.out.println("viso gero");//System.exit(0);
+                    break;
+                default:
+                    System.out.println("tokio veikmso nera");
+            }
         }
     }
-    private void countTriangle(Scanner scanner){
+
+    private void countTriangle(Scanner scanner) {
         System.out.println("iveskite pirma statini");
-        int a = scanner.nextInt();
+        double a = getCorrectNumber(scanner);
         System.out.println("iveskite antra statini");
-        int b = scanner.nextInt();
+        double b = getCorrectNumber(scanner);
 
         System.out.println("trikampo plotas yra " + a * b / 2);
 
     }
-    private void countRectangle(Scanner scanner){
+
+    private void countRectangle(Scanner scanner) {
         System.out.println("iveskite pirma staciakampio ilgi");
-        int a = scanner.nextInt();
+        double a = getCorrectNumber(scanner);
         System.out.println("iveskite antra staciakampo ploti");
-        int b = scanner.nextInt();
+        double b = getCorrectNumber(scanner);
 
         System.out.println("staciakampio plotas yra " + a * b);
     }
-    private void countSquare(Scanner scanner){
+
+    private void countSquare(Scanner scanner) {
         System.out.println("iveskite kvadrato ilgi");
-        int a = scanner.nextInt();
+        double a = getCorrectNumber(scanner);
         double z = Math.pow(a, 2);
 
         System.out.println("kvadrato plotas yra " + z);
     }
-    private void countCircle(Scanner scanner){
-        System.out.println("iveskite spindulio ilgi");
-        int a = scanner.nextInt();
-        double z = Math.pow(a, 2);
 
-        System.out.println("apskritimo plotas yra " + z * 3.1415);
+    private void countCircle(Scanner scanner) {
+        System.out.println("iveskite spindulio ilgi");
+        double a = getCorrectNumber(scanner);
+        DecimalFormat df = new DecimalFormat("0.00");
+        double z = Math.pow(a, 2);
+        double pi = 3.1415;
+
+        System.out.println("apskritimo plotas yra " + z * pi);
     }
 
-
+    private double getCorrectNumber(Scanner scanner) {
+        while (true)
+        {
+            try {
+                double number = scanner.nextDouble();
+                return number;
+            } catch (InputMismatchException ex) {
+                System.out.print("Ivedete bloga skaiciu pakartokite");
+                scanner.nextLine();
+            }
+        }
+    }
 }
