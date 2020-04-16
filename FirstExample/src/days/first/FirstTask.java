4aa750ee47199b1a9aae8c0a45ffeaa6693380e2
Mantasma <mant4s.mazeika@gmail.com>
2020-04-16 19:37:11 +0300

    add few new lines

--- FirstExample/src/days/first/FirstTask.java
+++ FirstExample/src/days/first/FirstTask.java
@@ -4,6 +4,7 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class FirstTask {
+
     public static void main(String[] args) {
         System.out.println("Iveskite zodi");
         Scanner sc = new Scanner(System.in);
